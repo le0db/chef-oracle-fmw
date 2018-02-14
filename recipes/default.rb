acb6c302f2c79bd1a7e1fd9fa0ffb4881ebc2c44
le0db <leonardo.busso1417@gmail.com>
2018-02-14 17:54:51 +0000

    updated 14/01

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,3 +9,15 @@ execute 'att_os' do
 	command 'yum update -y'
 	user 'root'
 end
+
+bash 'swap_space' do
+	code <<-EOH
+		sudo dd if=/dev/zero of=/swapfile bs=1024 count=756k
+		sudo mkswap /swapfile
+		sudo swapon /swapfile
+		echo /swapfile          swap            swap    defaults        0 0 >> /etc/fstab
+		sudo chown root:root /swapfile
+		sudo chmod 0600 /swapfile
+		EOH
+end
+		
